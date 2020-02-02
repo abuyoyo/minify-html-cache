c380f875833aaa4a0d5d00272597d420a9951f20
abuyoyo <abuyoyo@users.noreply.github.com>
2020-02-02 18:48:19 +0200

    license file

--- WPSCMin.php
+++ WPSCMin.php
@@ -1,21 +1,4 @@
 <?php
-
-/*  Copyright 2008-2015 Joel Hardi
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License, version 2, as 
-    published by the Free Software Foundation.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
-
 /*  class WPSCMin
  *
  *  Singleton class add-on to WP Super Cache to interface with HTML Minify
